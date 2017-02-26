dac939983f846e704d0cb24ccc01a3a425296ffe
DanilBaibak <danil.baibak@gmail.com>
2017-02-26 21:10:46 +0100

    Code style improvements

--- lstm.py
+++ lstm.py
@@ -12,31 +12,32 @@ warnings.filterwarnings("ignore")
 
 
 def load_data(filename, seq_len, normalise_window):
-    f = open(filename, 'r').read()
-    data = f.split('\n')
+    with open(filename, 'r') as file:
+        f = file.read()
+        data = f.split('\n')
 
-    sequence_length = seq_len + 1
-    result = []
-    for index in range(len(data) - sequence_length):
-        result.append(data[index: index + sequence_length])
+        sequence_length = seq_len + 1
+        result = []
+        for index in range(len(data) - sequence_length):
+            result.append(data[index: index + sequence_length])
 
-    if normalise_window:
-        result = normalise_windows(result)
+        if normalise_window:
+            result = normalise_windows(result)
 
-    result = np.array(result)
+        result = np.array(result)
 
-    row = round(0.9 * result.shape[0])
-    train = result[:int(row), :]
-    np.random.shuffle(train)
-    x_train = train[:, :-1]
-    y_train = train[:, -1]
-    x_test = result[int(row):, :-1]
-    y_test = result[int(row):, -1]
+        row = round(0.9 * result.shape[0])
+        train = result[:int(row), :]
+        np.random.shuffle(train)
+        x_train = train[:, :-1]
+        y_train = train[:, -1]
+        x_test = result[int(row):, :-1]
+        y_test = result[int(row):, -1]
 
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-    return [x_train, y_train, x_test, y_test]
+        return [x_train, y_train, x_test, y_test]
 
 
 def normalise_windows(window_data):
@@ -53,16 +54,17 @@ def build_model(layers):
     model.add(LSTM(
         input_dim=layers[0],
         output_dim=layers[1],
-        return_sequences=True))
+        return_sequences=True
+    ))
     model.add(Dropout(0.2))
 
     model.add(LSTM(
         layers[2],
-        return_sequences=False))
+        return_sequences=False
+    ))
     model.add(Dropout(0.2))
 
-    model.add(Dense(
-        output_dim=layers[3]))
+    model.add(Dense(output_dim=layers[3]))
     model.add(Activation("linear"))
 
     start = time.time()
